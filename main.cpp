d25e088ca649c1186706df8bc89f035aa52bac45
a-simanov <metall704@yandex.ru>
2025-07-19 21:23:03 +0300

    refactoring code

--- main.cpp
+++ main.cpp
@@ -10,25 +10,25 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     std::string find_str;
     std::cin >> find_str;
-    std::string finder = "\\s*(" + find_str + ")\\s*";
+
+    std::string finder = "\\s*(" + find_str + ")\\s*"; // для регулярного выражения
     QRegularExpression reg(QString::fromStdString(finder));
     std::smatch match;
+
     QDir cur_dir = QDir::current();
-    qDebug() << cur_dir;
     QFileInfoList entries = cur_dir.entryInfoList(QDir::Files);
-    bool succses = false;
-    int count = 0;
+    bool success = false; //для счетчика переименований
+    int count = 0;// счетчик переименований
+
     for (QFileInfo& entry : entries) {
         QRegularExpressionMatch match = reg.match(entry.fileName());
         if (match.hasMatch()) {
             QString old_name = entry.absoluteFilePath();
-            qDebug() << old_name;
             QString new_name = entry.fileName().remove(match.captured(0));
-            qDebug() << new_name;
-            succses = QFile::rename(old_name, new_name);
+            success = QFile::rename(old_name, new_name);
         }
-        count = succses ? (count + 1) : count;
-        succses = false;
+        count = success ? (count + 1) : count;
+        success = false;
     }
     std::cout << "Переименовано " << count << " файл(ов)";
     return 0;
